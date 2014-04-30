5ea7a7eb9caffbb2e6a9e35efb7fbfbc8924f6b7
Will Thames <will@thames.id.au>
2014-04-30 11:02:03 +1000

    Fixes to make it work.
    Better comments to stop me getting tangled in XML elements
    Also has a main now

--- src/books/core.clj
+++ src/books/core.clj
@@ -1,19 +1,24 @@
-;; we want to the price of the books by author XYZ
-(ns books.core)
+;; Print the titles of all books by Eva Corets
+(ns books.core
+  (:gen-class :main true))
 
 (require '[clojure.xml :as xml])
 
 (defn parsefile [s] (xml/parse (clojure.java.io/file s)))
 
-;; returns all tags named <tag>
+;; returns all sub elements named <tag>
+;; of an element with :tag, :attr, :content
 (defn elt-get-tags-named [elt tag]
-  (filter (fn [x] (= x tag)) (get elt :tag)))
+  (filter 
+    (fn [x] (= (get x :tag) tag))
+    (get elt :content)
+  ))
 
 ;; returns true if an element has a tag named <tag>
 ;; with content (e.g. <tag>content</tag>
 (defn elt-has-tag-with-content [elt tag content]
   (some 
-     (fn [x] (= (get x :content) content)) 
+     (fn [x] (= (get x :content) [content])) 
      (elt-get-tags-named elt tag))
   )
 
@@ -21,8 +26,16 @@
 
 (def books (get catalog :content))
 
+;; eva is all the books with author Eva Corets
 (def eva 
   (filter 
-    (fn [x] (elt-has-tag-with-content (get x :content) :author "Corets, Eva")) 
+    (fn [x] (elt-has-tag-with-content x :author "Corets, Eva")) 
     books))
 
+;; print the content of the first title element for each book in eva
+(defn -main [& args]
+  (println 
+    (map 
+      (fn[x] (get (first (elt-get-tags-named x :title)) :content)) 
+      eva)))
+  
